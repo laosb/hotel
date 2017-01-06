4ffdb147ea07c098d61c849849d49f75658daf3d
Wexpo Lyu <laosb@lns.unihubs.com>
2017-01-07 06:25:09 +0800

    Update and rename create.js to init.js

---
+++ src/cli/init.js
@@ -0,0 +1,12 @@
+const { hotelDir, confFile } = require('../common');
+const defaults = require('../conf-defaults');
+
+function init() {
+  if (!fs.existsSync(confFile)) {
+    console.log("It's already a project!");
+    process.exit(1);
+  }
+  const data = JSON.stringify(defaults, null, 2);
+  fs.writeFileSync(confFile, data);
+  console.log("Successfully created your project.")
+}
